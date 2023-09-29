a3ce3d21012df278d637847496426fdc06ce8349
MishaKamal <145194684+MishaKamal@users.noreply.github.com>
2023-09-30 00:10:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin: 'top'});
+sr.reveal('.hero-imgg', {delay:450, origin: 'top'});
